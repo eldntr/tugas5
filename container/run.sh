a838f0de81a1e9a2394623e95e150a9bd42c5d2d
Eldintaro Farrandi <eldintarofarrandi@gmail.com>
2024-12-20 13:50:33 +0700

    modified:   .github/workflows/deploy.yml 	modified:   container/run.sh

--- container/run.sh
+++ container/run.sh
@@ -1,10 +1,12 @@
 docker stop nginx-rtmp-server || true
 docker rm nginx-rtmp-server || true
 
+# Build the Docker image
+docker build -t nginx-rtmp-server:latest .
+
 docker run -d --name nginx-rtmp-server \
     -p 8080:80 \
     -p 1935:1935 \
     -p 5000:5000 \
     nginx-rtmp-server
 
-    .github/workflows
